033b641e60b8d9d3f0c3f2392fb24cfa9bb2dcd0
wzj <tojunjunwei@gmail.com>
2018-12-26 11:24:32 +0800

    first commit

---
+++ src/Comp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/26
+ * Time: 11:08
+ */
+namespace Comp;
+
+class Comp
+{
+    public function hello() {
+        echo "hello";
+    }
+}
