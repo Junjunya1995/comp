608d4f2cf5ec9a1488631e8ec22b9bc263938172
wzj <tojunjunwei@gmail.com>
2018-12-27 15:39:26 +0800

    commit

---
+++ src/Upload.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/26
+ * Time: 11:08
+ */
+namespace EasySwoole\Upload;
+
+class Upload
+{
+    public function hello() {
+        echo "hello";
+    }
+
+    public function say() {
+        echo "wawa";
+    }
+}
